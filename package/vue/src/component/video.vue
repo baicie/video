f51520433b3f5d95242bd988f4894ded9a531824
white-cie <2055572879@qq.com>
2023-05-10 00:03:24 +0800

    feat(custom): 增加play 增加vueplay

---
+++ package/vue/src/component/video.vue
@@ -0,0 +1,29 @@
+<script lang='ts' setup>
+// # config import
+import videojs from 'video.js'
+import { onMounted } from 'vue'
+import { PLAYER_ID } from '../util'
+
+// # base state
+// # pinia
+// # computed
+// # watch
+// # function
+function main() {
+  const player = videojs(PLAYER_ID)
+  console.log(player)
+}
+// # lifecycle
+onMounted(() => {
+  main()
+})
+// # style
+</script>
+
+<template>
+  <div class="@baicie-video-vue">
+    <video :id="PLAYER_ID" />
+  </div>
+</template>
+
+<style lang='scss' scoped></style>
